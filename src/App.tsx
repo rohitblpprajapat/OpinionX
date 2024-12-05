387a5e1f9b5458992390fd6564a2a1532400d3c9
blprohit <rohitblpprajapat@gmail.com>
2024-12-06 05:04:51 +0530

    Add game cards to App component, update Navbar layout, and enhance Card styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,32 @@
 import Card from "./components/cards";
 import Navbar from "./components/Navbar";
 
+const games = ([
+	{
+	  title: 'Basketball Championship',
+	  description: 'Bet on the winning team.',
+	  odds: '2.5x',
+	  image: 'src/assets/Logo 640x640.png' 
+	},
+	{
+	  title: 'Soccer World Cup',
+	  description: 'Predict the winner of the tournament.',
+	  odds: '1.8x',
+	  image: 'src/assets/Logo 640x640.png' 
+	},
+	{
+	  title: 'us election',
+	  description: 'Predict the winner of the election.',
+	  odds: '1.8x',
+	  image: 'src/assets/Logo 640x640.png' 
+	},
+	{
+	  title: 'Formula 1 Racing',
+	  description: 'Bet on the fastest lap time.',
+	  odds: '3.0x',
+	  image: 'src/assets/Logo 640x640.png' 
+	}
+  ])
 
 function App() {
 	
@@ -8,8 +34,16 @@ function App() {
 	return (
 		<>
 			<Navbar />
-			
-			<Card title="Sample Title" description="Sample Description" />
+			<section className="w-full flex flex-col justify-center mt-20">
+				<div className="flex justify-center flex-wrap px-2 my-12">
+					{games.map((item, index) => (
+						<Card 
+						key={index}
+						title={item.title}
+						description={item.description} />
+					))}
+				</div>
+			</section>
 		</>
 	);
 }
--- src/components/cards.tsx
+++ src/components/cards.tsx
@@ -8,17 +8,18 @@ interface CardProps {
 
 const Card: React.FC<CardProps> = ({ title, description }) => {
     return (
-        <div className="flex flex-col justify-center w-60 h-[300px] border-2 rounded-2xl">
-            <div>
+        <div className="px-5 ease-in-out transition duration-300 flex flex-col justify-between w-60 h-[300px] border-2 rounded-2xl hover:shadow-lg hover:scale-105">
+            <div className="mx-2 mt-2">
                 <h2 className="font-bold">{title}</h2>
                 <p>{description}</p>
             </div>
-            <div>
-                <button className="px-5 py-2 rounded-xl bg-green-500">Yes</button>
-                <button className="px-5 py-2 rounded-xl bg-red-500 ">No</button>
+            <div className="flex justify-between px-5 py-2">
+                <button className="px-5 py-2 rounded-xl bg-green-500 text-white">Yes</button>
+                <button className="px-5 py-2 rounded-xl bg-red-500 text-white">No</button>
             </div>
         </div>
     );
 };
+
 
 export default Card;
